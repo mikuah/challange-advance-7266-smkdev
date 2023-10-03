2658b92e2c1bf7f299379f80b6073aa799c8ddba
Iqbal Pasha <iqbalpasha6a@gmail.com>
2023-10-03 11:11:33 +0700

    add advance challenge 8142

---
+++ challenge-7644/advance/index.js
@@ -0,0 +1,5 @@
+n = 5;
+
+for (i = 0; i < n; i++) {
+  console.log("2");
+}
---
+++ challenge-8142/advance/index.js
@@ -0,0 +1,6 @@
+let input_number = 6;
+
+for (i = 1; i <= input_number; i++) {
+  let result = i * i * i;
+  console.log(`Current Number is : ${i} and the cube is ${result}`);
+}
